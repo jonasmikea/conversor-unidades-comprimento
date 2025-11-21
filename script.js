355b5b52b83ac0e272a7342ab1a03fe658d4d9d7
jonasmikea <jonamikeafilho00@gmail.com>
2025-11-21 13:12:38 -0300

    feat: adiciona captura de dados e validacao

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function converter() {
-    console.log("Função iniciada");
+    const valorInput = document.getElementById('valor').value;
+    const unidadeOrigem = document.getElementById('unidadeOrigem').value;
+    const unidadeDestino = document.getElementById('unidadeDestino').value;
+
+    if (!valorInput) {
+        alert('Digite um valor!');
+        return;
+    }
 }
